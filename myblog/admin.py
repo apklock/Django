55fae987f73ac3877fc78ccef3ce0cde920f206b
apklock <apklock@gmail.com>
2015-02-25 16:21:44 -0800

    adding code in admin.py for ModelAdmin

--- myblog/admin.py
+++ myblog/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from myblog.models import Post
 from myblog.models import Category
+from myblog.models import Author
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    pass
 
 
 admin.site.register(Post)
 admin.site.register(Category)
-
+admin.site.register(Author, AuthorAdmin)
